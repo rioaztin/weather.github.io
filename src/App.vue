7adaf1cf50b93616340f967701ab8e3a61f67536
Egi Ramadhan <egi@redcomm.co.id>
2022-04-05 11:54:10 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,26 @@
 <div id="app">
   <main>
     <div class="search-box">
-      <input type="text" class="search-bar" placeholder="Search...">
-
+      <input 
+      type="text" 
+      class="search-bar" 
+      placeholder="Search..."
+      v-model="query"
+      @keypress="fetchWeather"
+      >
     </div>
+
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'"> 
+        <div class="location-box">
+          <div class="location">{{ weather.name }}, {{ weather.sys.country}}</div>
+          <div class="date">{{ dateBuilder() }}</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
+        </div>
+      </div>
   </main>
 
 </div>
@@ -16,8 +33,35 @@ export default {
   name: 'App',
   data () {
     return {
-      api_key: '73210f1a9a1c06f189752f9c4181c4b7'
+      api_key: '73210f1a9a1c06f189752f9c4181c4b7',
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
     }
+  },
+  methods: {
+    fetchWeather (e) {
+      if (e.key == "Enter") {
+         fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+        .then(res => {
+          return res.json();
+        }).then(this.setResults);
+      }
+    },
+      setResults (results) {
+        this.weather = results; 
+      },
+      dateBuilder () {
+        let d = new Date();
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+        let day = days[d.getDay()];
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear();
+        return `${day} ${date} ${month} ${year}`;
+      }
   }
 }
 </script>
@@ -70,5 +114,47 @@ main {
   transition: 0.4s;
 }
 
+.location-box .location {
+  color: #ffff;
+  font-size: 32px;
+  font-weight: 600;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .date {
+  color: #ffff;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 102px;
+  font-weight: 900;
+
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather {
+  color: #fff;
+  font-size: 48px;
+  font-weight: 600;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
 
 </style>
